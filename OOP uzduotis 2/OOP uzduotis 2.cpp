7d612e2a763428b01076b70fdc7a1d5af795a468
Marius Bieliauskas <mariux454@gmail.com>
2018-04-05 12:21:15 +0300

    nebereikia kartoti ivesties

--- OOP uzduotis 2/OOP uzduotis 2.cpp
+++ OOP uzduotis 2/OOP uzduotis 2.cpp
@@ -57,91 +57,89 @@ int main()
 		failu_kurimas(sk);
 		goto pradzia;
 	}
+	auto start = high_resolution_clock::now();
+	std::vector<studentai> stud;
+	std::list<studentai> stud1;
+	std::deque<studentai> stud2;
+	unsigned int m = 0, nd = 0;
+	if (nr == 1)
+	{
+		std::cout << "iveskite kiek bus vedama mokiniu: ";
+		bool bad = false;
 
-	for (unsigned int k = 0; k < 6; k++) {
-		auto start = high_resolution_clock::now();
-		std::vector<studentai> stud;
-		std::list<studentai> stud1;
-		std::deque<studentai> stud2;
+		do {
+			std::cin >> m;
 
-		unsigned int m = 0, nd = 0;
+			bad = std::cin.fail();
+			if (bad)
+				std::cout << "Tai nera skaicius, bandykite dar karta: " << std::endl;
+			std::cin.clear();
+			std::cin.ignore(10, '\n');
+		} while (bad);
+		bad = false;
 
+		std::cout << "iveskite kiek bus vedama nd pazymiu: ";
+		do {
+			std::cin >> nd;
 
-		if (nr == 1)
+			bad = std::cin.fail();
+			if (bad)
+				std::cout << "Tai nera skaicius, bandykite dar karta: " << std::endl;
+			std::cin.clear();
+			std::cin.ignore(10, '\n');
+		} while (bad);
+		bad = false;
+
+		stud.reserve(m);
+		for (unsigned int i = 0; i < m; i++)
 		{
-			std::cout << "iveskite kiek bus vedama mokiniu: ";
-			bool bad = false;
-
-			do {
-				std::cin >> m;
-
-				bad = std::cin.fail();
-				if (bad)
-					std::cout << "Tai nera skaicius, bandykite dar karta: " << std::endl;
-				std::cin.clear();
-				std::cin.ignore(10, '\n');
-			} while (bad);
-			bad = false;
-
-			std::cout << "iveskite kiek bus vedama nd pazymiu: ";
-			do {
-				std::cin >> nd;
-
-				bad = std::cin.fail();
-				if (bad)
-					std::cout << "Tai nera skaicius, bandykite dar karta: " << std::endl;
-				std::cin.clear();
-				std::cin.ignore(10, '\n');
-			} while (bad);
-			bad = false;
-
-			stud.reserve(m);
-			for (unsigned int i = 0; i < m; i++)
+			studentai temp;
+			std::cout << "iveskite " << i + 1 << "-ojo mokinio(es) pavarde: ";
+			std::cin >> temp.pavarde;
+			std::cout << "iveskite " << i + 1 << "-ojo mokinio(es) varda: ";
+			std::cin >> temp.vardas;
+			std::cout << "iveskite " << i + 1 << "-ojo mokinio(es) " << nd << " namu darbu pazymius:" << std::endl;
+			for (unsigned int j = 0; j < nd; j++)
 			{
-				studentai temp;
-				std::cout << "iveskite " << i + 1 << "-ojo mokinio(es) pavarde: ";
-				std::cin >> temp.pavarde;
-				std::cout << "iveskite " << i + 1 << "-ojo mokinio(es) varda: ";
-				std::cin >> temp.vardas;
-				std::cout << "iveskite " << i + 1 << "-ojo mokinio(es) " << nd << " namu darbu pazymius:" << std::endl;
-				for (unsigned int j = 0; j < nd; j++)
-				{
-					unsigned short int a;
-					int test = 0;
-					do {
-						std::cin >> a;
-
-						bad = std::cin.fail();
-						if (bad || a > 10)
-							std::cout << "Tai nera skaicius arba skaicius nera tarp 0 ir 10, bandykite dar karta: " << std::endl;
-						std::cin.clear();
-						std::cin.ignore(10, '\n');
-					} while (bad);
-					bad = false;
-
-				}
-				std::cout << "iveskite " << i + 1 << "-ojo mokinio egzamino bala: ";
+				unsigned short int a;
+				int test = 0;
 				do {
-					std::cin >> temp.egz;
+					std::cin >> a;
 
 					bad = std::cin.fail();
-					if (bad || temp.egz > 10)
+					if (bad || a > 10)
 						std::cout << "Tai nera skaicius arba skaicius nera tarp 0 ir 10, bandykite dar karta: " << std::endl;
 					std::cin.clear();
 					std::cin.ignore(10, '\n');
 				} while (bad);
 				bad = false;
-
-				if (k == 0 || k == 3)
-					stud.push_back(temp);
-				else if (k == 1 || k == 4)
-					stud1.push_back(temp);
-				else
-					stud2.push_back(temp);
-				start = high_resolution_clock::now();
+				temp.paz.push_back(a);
 			}
+			std::cout << "iveskite " << i + 1 << "-ojo mokinio egzamino bala: ";
+			do {
+				std::cin >> temp.egz;
+
+				bad = std::cin.fail();
+				if (bad || temp.egz > 10)
+					std::cout << "Tai nera skaicius arba skaicius nera tarp 0 ir 10, bandykite dar karta: " << std::endl;
+				std::cin.clear();
+				std::cin.ignore(10, '\n');
+			} while (bad);
+			bad = false;
+
+				stud.push_back(temp);
+				stud1.push_back(temp);
+				stud2.push_back(temp);
 		}
-		else
+	}
+
+	for (unsigned int k = 0; k < 6; k++) {
+		start = high_resolution_clock::now();
+		
+
+		
+
+		if (nr == 2)
 		{
 
 			std::ifstream fd("kursiokai.txt");
